33d40a9e24db8983314686f471a7812047bf3123
kametan0730 <14833564+kametan0730@users.noreply.github.com>
2024-08-20 10:31:13 +0900

    Windows対応

--- README.md
+++ README.md
@@ -2,9 +2,25 @@
 
 ### 共通
 - プログラムファイル内のインターフェース名(eth0)を環境に合わせて変更して下さい
-- 実行にはroot権限若しくはCAP_NET_RAWケーパビリティが必要です
 
 ### Python
+#### Windows
+- Python3とライブラリScapyのインストールが必要です
+    - https://www.python.org/downloads/
+    - https://scapy.net/
+- WindowsにWinPcapをインストールする必要があります
+    - Wiresharkのインストールウィザードの途中でWinPcapをインストール可能です
+    - https://www.wireshark.org/download.html
+
+
+```
+cd python3
+sudo python3 main_win.py
+```
+
+
+#### Linux
+- 実行にはroot権限若しくはCAP_NET_RAWケーパビリティが必要です
 ```
 cd python
 sudo python3 main.py
@@ -12,6 +28,8 @@ sudo python3 main.py
 
 
 ### C++
+#### Linux
+- 実行にはroot権限若しくはCAP_NET_RAWケーパビリティが必要です
 ```
 cd cpp
 sudo make run
---
+++ python/main_win.py
@@ -0,0 +1,46 @@
+from scapy.all import *
+import threading
+import time
+
+# イーサネットフレームの送信関数
+def send_frame(frame, iface):
+    while True:
+        sendp(frame, iface=iface, verbose=False)  # Scapyのsendp関数を使用
+        print(f"frame sent: {frame.summary()}")
+        time.sleep(1)
+
+# イーサネットフレームの受信関数
+def receive_frame(iface):
+    def packet_handler(packet):
+        # Ethernetレイヤーが存在するか確認
+        if packet.haslayer(Ether):
+            eth = packet.getlayer(Ether)
+            # 送信元MACアドレスがターゲットMACアドレスと一致するか確認
+            if eth.src != '00:0c:29:12:34:56':
+                print(f"Captured packet : {packet.summary()}")
+
+    sniff(iface=iface, prn=packet_handler, store=False)  # Scapyのsniff関数を使用
+
+# メイン関数
+def main():
+    interface = 'イーサネット 3'  # 使用するインターフェース名を指定
+
+    # 送信するイーサネットフレームを構築
+    dest_mac = 'ff:ff:ff:ff:ff:ff'  # ブロードキャストMACアドレス
+    src_mac = '00:0c:29:12:34:56'  # 自分のMACアドレス
+    eth_type = 0x0800  # IPプロトコル
+    payload = b'Hello, Ethernet!'
+    frame = Ether(dst=dest_mac, src=src_mac, type=eth_type) / payload
+
+    # スレッドを作成して送受信を並列に実行
+    send_thread = threading.Thread(target=send_frame, args=(frame, interface))
+    recv_thread = threading.Thread(target=receive_frame, args=(interface,))
+
+    send_thread.start()
+    recv_thread.start()
+
+    send_thread.join()
+    recv_thread.join()
+
+if __name__ == "__main__":
+    main()
